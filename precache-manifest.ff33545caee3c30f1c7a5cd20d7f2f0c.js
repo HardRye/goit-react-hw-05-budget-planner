53c725b43ac6753abe8d055222e97baedf3e3b39
HardRye <nikolaimihail@gmail.com>
2019-11-21 07:45:35 +0200

    Updates

---
+++ precache-manifest.ff33545caee3c30f1c7a5cd20d7f2f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e026f809f839db8c5418faad8c36db10",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "9983f4b347a3408263a1",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.656b51cb.chunk.css"
+  },
+  {
+    "revision": "b6ddb7440364adb45346",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.5e3e1def.chunk.js"
+  },
+  {
+    "revision": "9983f4b347a3408263a1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.16e1b6cc.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
